d1345619e649ef3ee378a683b4e2a7b7671b8db5
Gargantua <martinzb949@gmail.com>
2020-12-06 16:27:22 -0800

    used activity 12-15 to create this working app with the help of my TA

---
+++ routes/apiRoutes.js
@@ -0,0 +1,61 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+//table data = notes data & wait list data = index data
+// ===============================================================================
+
+//var notesData = require("../public/notes");
+//var indexData = require("../public/index");
+var fs = require("fs");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/notes", function(req, res) {
+        console.log("Hitting the log")
+        fs.readFile("./db/db.json", "utf-8", (err, data) => {
+            console.log(err, data)
+            const parse = JSON.parse(data);
+            console.log(parse);
+            res.json(parse);
+        })
+
+    });
+    //Hello World!
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // ---------------------------------------------------------------------------
+    app.post("/api/notes", function(req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body parsing middleware
+        console.log(req.body);
+        fs.readFile("./db/db.json", "utf-8", (err, data) => {
+            console.log(err, data)
+                //Chad - do something Here to add a unique id to the new parsedData; universally unique id? UUID? npm package4
+                //maybe Math.random?
+            const parseData = JSON.parse(data);
+            //parseData.id = "some unique id"
+            parseData.push(req.body)
+            fs.writeFile("./db/db.json", JSON.stringify(parseData), error => console.log(error));
+            res.json(true);
+
+        })
+    });
+    app.delete("/api/notes/:id", function(req, res) {
+        console.log(req.params.id)
+    })
+};
